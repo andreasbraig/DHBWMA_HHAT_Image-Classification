a4af560c79d4afba54f8117c961dcbd6c0daaf8d
andreasbraig <jpotze22@gmail.com>
2025-03-01 20:59:18 +0100

    Studienarbeit aktualisiert: neue Modelle hinzugefügt,Confusion Matrix hinzugefügt und Testskripte erstellt

--- training_prep.py
+++ training_prep.py
@@ -11,40 +11,41 @@ import pycore.preprocessing.filemanagement as ufm
 # ________________ DIESES FILE DIENT DER EINMALIGEN VORBEREITUNG ZUM TRAINING DES MODELLS_______________
 
 #JSON Settings Laden
-
-config_path = "pycore/setings.json"
-
-cf = json.load(open(config_path, 'r'))
-
-
-#___________Ausführender Bereich
-
-
-#Bilder zu Grayscale
-uic.folder_to_grayscale(cf["filepaths"]["good"],cf["filepaths"]["good_gray"])
-uic.folder_to_grayscale(cf["filepaths"]["bad"],cf["filepaths"]["bad_gray"])
-
-#Aufteilung der Daten
-#Gut
-ufm.split_data(source=cf["filepaths"]["good_gray"],
-               destination1=cf["filepaths"]["test"],
-               destination2=cf["filepaths"]["train"],
-               test_size=cf["ttsplit"]["test_size"],
-               random_state=cf["ttsplit"]["random_state"])
-
-#Schlecht
-ufm.split_data(source=cf["filepaths"]["bad_gray"],
-               destination1=cf["filepaths"]["test"],
-               destination2=cf["filepaths"]["train"],
-               test_size=cf["ttsplit"]["test_size"],
-               random_state=cf["ttsplit"]["random_state"])
-
-#Aufteilung der Train Daten in Train und Validate
-ufm.split_data(source=cf["filepaths"]["train"],
-               destination1=cf["filepaths"]["validate"],
-               destination2=cf["filepaths"]["train"],
-               test_size=cf["ttsplit"]["test_size"],
-               random_state=cf["ttsplit"]["random_state"])
-
-#Funktioniert bis hierhin! 
-
+if __name__ == "__main__":
+    config_path = "pycore/setings.json"
+    
+    cf = json.load(open(config_path, 'r'))
+    
+    
+    #___________Ausführender Bereich
+    
+    
+    #Bilder zu Grayscale
+    uic.folder_to_grayscale(cf["filepaths"]["good"],cf["filepaths"]["good_gray"])
+    uic.folder_to_grayscale(cf["filepaths"]["bad"],cf["filepaths"]["bad_gray"])
+    
+    #Aufteilung der Daten
+    #Gut
+    ufm.split_data(source=cf["filepaths"]["good_gray"],
+                   destination1=cf["filepaths"]["test"],
+                   destination2=cf["filepaths"]["train"],
+                   test_size=cf["ttsplit"]["test_size"],
+                   random_state=cf["ttsplit"]["random_state"])
+    
+    #Schlecht
+    ufm.split_data(source=cf["filepaths"]["bad_gray"],
+                   destination1=cf["filepaths"]["test"],
+                   destination2=cf["filepaths"]["train"],
+                   test_size=cf["ttsplit"]["test_size"],
+                   random_state=cf["ttsplit"]["random_state"])
+    
+    #Aufteilung der Train Daten in Train und Validate
+    ufm.split_data(source=cf["filepaths"]["train"],
+                   destination1=cf["filepaths"]["validate"],
+                   destination2=cf["filepaths"]["train"],
+                   test_size=cf["ttsplit"]["test_size"],
+                   random_state=cf["ttsplit"]["random_state"])
+    
+    #Funktioniert bis hierhin! 
+    
+    
