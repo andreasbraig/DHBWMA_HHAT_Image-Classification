b53af69c203fa718286cfe714e8c6b404e40ffe9
andreasbraig <jpotze22@gmail.com>
2025-03-14 18:36:42 +0100

    Füge neue Bilddateien für das Training und die Evaluierung hinzu

---
+++ main.py
@@ -0,0 +1,22 @@
+# import json
+# import numpy as np
+# from pycore.preprocessing import imageconversion as uic
+# from pycore.classification import classification as cls
+
+# config_path = "pycore/setings.json"
+
+# cf = json.load(open(config_path, 'r'))
+
+# image_path="Bilder/Bad_Pictures/Bad (13).jpg"
+# #image_path="Bilder/Good_Pictures/Good (6).jpg"
+
+# model = cls.get_Model("mobilenet_v1.h5")
+
+# binary_latest = uic.single_to_grayscale(image_path,"Bilder/Cache_Grayscale","test.jpg")
+
+
+# img = preprocess_image(image_path)
+
+# result = cls.classify_image(img,model)
+
+# print(result)
